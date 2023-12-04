5d7fae772c67003660bddaabf8c327adca265ef7
Taniya-eGov <taniya@egovernments.org>
2023-12-04 22:31:16 +0530

    Added getAllPosts in PostController

--- src/main/java/com/springboot/blogapp/controller/PostController.java
+++ src/main/java/com/springboot/blogapp/controller/PostController.java
@@ -4,10 +4,9 @@ import com.springboot.blogapp.payload.PostDto;
 import com.springboot.blogapp.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -23,5 +22,10 @@ public class PostController {
     @PostMapping
     public ResponseEntity<PostDto> createPost(@RequestBody PostDto postDto){
         return new ResponseEntity<>(postService.createPost(postDto), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<PostDto> getAllPosts(){
+        return postService.getAllPosts();
     }
 }
