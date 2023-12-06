1a3591f9c5efb2462b74070334ec389a64a0f472
Taniya-eGov <taniya@egovernments.org>
2023-12-06 18:49:41 +0530

    updateComment implementation

--- src/main/java/com/springboot/blogapp/controller/CommentController.java
+++ src/main/java/com/springboot/blogapp/controller/CommentController.java
@@ -37,4 +37,10 @@ public class CommentController {
         CommentDto commentDto=commentService.getCommentById(postId, commentId);
         return new ResponseEntity<>(commentDto,HttpStatus.OK);
     }
+
+    @PutMapping("/posts/{postId}/comments/{commentId}")
+    public ResponseEntity<CommentDto> updateComment(@PathVariable long postId, @PathVariable long commentId, @RequestBody CommentDto commentDto) {
+        CommentDto updatedComment=commentService.updateComment(postId, commentId, commentDto);
+        return new ResponseEntity<>(updatedComment,HttpStatus.OK);
+    }
 }
--- src/main/java/com/springboot/blogapp/service/impl/CommentServiceImpl.java
+++ src/main/java/com/springboot/blogapp/service/impl/CommentServiceImpl.java
@@ -68,6 +68,28 @@ public class CommentServiceImpl implements CommentService {
         return convertEntityToDto(comment);
     }
 
+    @Override
+    public CommentDto updateComment(long postId, long commentId, CommentDto commentDto) {
+        //retrieve post entity by id
+        Post post=postRepository.findById(postId).orElseThrow(
+                () -> new ResourceNotFoundException("Post","id",postId));
+
+        //retrieve comment entity by id
+        Comment comment=commentRepository.findById(commentId).orElseThrow(
+                () -> new ResourceNotFoundException("Comment","id",commentId));
+
+        if(!comment.getPost().getId().equals(post.getId())){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST,"Comment does not belongs to post");
+        }
+
+        comment.setName(commentDto.getName());
+        comment.setEmail(commentDto.getEmail());
+        comment.setBody(commentDto.getBody());
+
+        Comment updatedComment=commentRepository.save(comment);
+        return convertEntityToDto(updatedComment);
+    }
+
 
     private Comment convertDtoToEntity(CommentDto commentDto){
         Comment comment=new Comment();
