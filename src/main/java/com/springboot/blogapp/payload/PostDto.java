519e9f33880c2fe56dcbfe3b6530b7c179c0cabe
Taniya1493 <taniyapushey@gmail.com>
2024-02-03 18:55:52 +0530

    Customising Swagger Models Documentation with Annotations

--- src/main/java/com/springboot/blogapp/payload/PostDto.java
+++ src/main/java/com/springboot/blogapp/payload/PostDto.java
@@ -1,6 +1,7 @@
 package com.springboot.blogapp.payload;
 
 import com.springboot.blogapp.entity.Comment;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
@@ -8,20 +9,35 @@ import lombok.Data;
 import java.util.Set;
 
 @Data
+@Schema(
+        description = "PostDto Model Information"
+)
 public class PostDto {
     private long id;
 
+    @Schema(
+            description = "Blog Post Title"
+    )
     @NotEmpty
     @Size(min = 2,message = "Post title should have maximum 2 characters")
     private String title;
 
+    @Schema(
+            description = "Post description"
+    )
     @NotEmpty
     @Size(min = 2,message = "Post Description should have maximum 2 characters")
     private String description;
 
+    @Schema(
+            description = "Blog Post Content"
+    )
     @NotEmpty
     private String content;
     private Set<CommentDto> comments;
 
+    @Schema(
+            description = "Blog Post category"
+    )
     private Long categoryId;
 }
